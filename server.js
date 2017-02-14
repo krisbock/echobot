b79a8ed66228303efccd2f381dab49717bc203d3
Kris Bock <krisbock@hotmail.com>
2017-02-14 22:28:55 +1000

    Rewrote with new API

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-// Get secrets from server environment
-var botConnectorOptions = { 
-    MicrosoftAppId : process.env.BOTFRAMEWORK_APPID, 
-    MicrosoftAppPassword: process.env.BOTFRAMEWORK_APPSECRET 
-};
+//=========================================================
+// Bot Setup
+//=========================================================
 
-// Create bot
-var bot = new builder.BotConnectorBot(botConnectorOptions);
-bot.add('/', function (session) {
-    
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    MicrosoftAppId: process.env.BOTFRAMEWORK_APPID,
+    MicrosoftAppPassword: process.env.BOTFRAMEWORK_APPSECRET 
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
     //respond with user's message
     session.send("You said " + session.message.text);
 });
 
-// Setup Restify Server
-var server = restify.createServer();
-
-// Handle Bot Framework messages
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-
 // Serve a static web page
 server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
+    'directory': '.',
+    'default': 'index.html'
 }));
-
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
-});
